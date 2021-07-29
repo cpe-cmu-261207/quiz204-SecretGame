c49655d94de68097b0c2b0627e8df92c05e0a28e
winnapat <GGgun2013@gmail.com>
2021-07-29 14:19:38 +0700

    test push

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
 // define more constants and variables here
+const btn_search = document.querySelector('#search')
 
 btn_toggle.onclick = () => {
   const name = author.innerHTML
   let number = /\d+/
-  author.innerHTML = name.match(number)
+  author.innerHTML = name.match(number)-length.innerHTML
 }
+
+
 
 // more codes for Search and Reset buttons here
